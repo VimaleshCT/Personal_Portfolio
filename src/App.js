5827fdb9ae3b5ac0603ec6122db2170586b0ad0e
Vimalesh C T <pranovvimal30@gmail.com>
2024-01-01 16:41:42 +0530

    intermediate commit of Portfolio

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from "../src/assets/img/pro2.png";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { NavBar } from "./components/NavBar";
 import { Banner } from "./components/Banner";
 import { Skills } from "./components/Skills";
