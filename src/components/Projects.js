dbfd0e70b105d5c36bc04c938e2fb8c5304c71fe
Vimalesh C T <pranovvimal30@gmail.com>
2024-01-07 18:40:14 +0530

    Added Certificate portion

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,6 +11,14 @@ import p8 from "../assets/img/project_8.png";
 import p9 from "../assets/img/project_9.png";
 import p10 from "../assets/img/project_9.jpeg";
 import p11 from "../assets/img/project_10.png";
+import p12 from "../assets/img/project_11.png";
+import p13 from "../assets/img/project_12.png";
+import c1 from "../assets/img/cert1.jpg";
+import c2 from "../assets/img/cert2.png";
+import c3 from "../assets/img/cert3.png";
+import c4 from "../assets/img/cert4.png";
+import c5 from "../assets/img/cert5.png";
+import c6 from "../assets/img/cert6.png";
 
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import "animate.css";
@@ -22,6 +30,16 @@ export const Projects = () => {
       title: "STARFLIX",
       description: "Movie Suggestion Website (Java,HTML,CSS)",
       imgUrl: p3,
+    },
+    {
+      title: "Weather",
+      description: "Realtime Weather App (React,HTML,CSS)",
+      imgUrl: p12,
+    },
+    {
+      title: "CT_Chat",
+      description: "Realtime Chat App (React,Socker.io,node js)",
+      imgUrl: p13,
     },
     {
       title: "Caterpillar Game",
@@ -74,6 +92,38 @@ export const Projects = () => {
       imgUrl: p6,
     },
   ];
+  const certificate = [
+    {
+      title: "Sentiment Analysis with NLP using Python and Flask",
+      Issuedby: " UDEMY ",
+      imgUrl: c1,
+    },
+    {
+      title: "Python - Data Analytics - Real World Hands-on Projects",
+      Issuedby: " UDEMY ",
+      imgUrl: c2,
+    },
+    {
+      title: "React jS for Be inners",
+      Issuedby: " Simplilearn ",
+      imgUrl: c3,
+    },
+    {
+      title: "Python And Django Framework For Beginners Complete Course",
+      Issuedby: " UDEMY ",
+      imgUrl: c4,
+    },
+    {
+      title: "Command Line Interface (CLI) With Golang From Infosys",
+      Issuedby: " Coursera ",
+      imgUrl: c5,
+    },
+    {
+      title: "The Complete JavaScript Course 2023: From Zero to Expert!",
+      Issuedby: " UDEMY ",
+      imgUrl: c6,
+    },
+  ];
 
   return (
     <section className="project" id="projects">
@@ -87,18 +137,6 @@ export const Projects = () => {
                     isVisible ? "animate__animated animate__fadeIn" : ""
                   }
                 >
-                  <h2>Projects</h2>
-                  <p>
-                    Learning enthusiastic, passionate about technology and
-                    innovation, eager to engage myself with tech events,
-                    workshops and projects. Looking forward to enhancing my
-                    skills, explore new challenges, and grow professionally.
-                    <br></br>I have a great passion to do programming, develop
-                    web apps, and websites . My goal is to utilize my skills for
-                    the development and growth of my organization with the help
-                    of new technologies. Looking forward to enhancing my skills,
-                    explore new challenges, and grow professionally.
-                  </p>
                   <Tab.Container id="projects-tabs" defaultActiveKey="first">
                     <Nav
                       variant="pills"
@@ -122,6 +160,20 @@ export const Projects = () => {
                       }
                     >
                       <Tab.Pane eventKey="first">
+                        <h2>Projects</h2>
+                        <p>
+                          Learning enthusiastic, passionate about technology and
+                          innovation, eager to engage myself with tech events,
+                          workshops and projects. Looking forward to enhancing
+                          my skills, explore new challenges, and grow
+                          professionally.
+                          <br></br>I have a great passion to do programming,
+                          develop web apps, and websites . My goal is to utilize
+                          my skills for the development and growth of my
+                          organization with the help of new technologies.
+                          Looking forward to enhancing my skills, explore new
+                          challenges, and grow professionally.
+                        </p>
                         <Row>
                           {projects.map((project, index) => {
                             return <ProjectCard key={index} {...project} />;
@@ -129,7 +181,32 @@ export const Projects = () => {
                         </Row>
                       </Tab.Pane>
                       <Tab.Pane eventKey="second">
-                        <p></p>
+                        <h2>Certificates</h2>
+                        <p>
+                          In pursuit of expanding my expertise in the realm of
+                          technology and programming, I have completed several
+                          significant certifications. These include
+                          certifications in fields such as web development,
+                          where I obtained a certificate in Django,
+                          demonstrating my proficiency in building robust web
+                          applications using Python. Additionally, I have
+                          acquired a certificate in Angular, showcasing my
+                          skills in developing dynamic and responsive front-end
+                          applications.<br></br> To further enhance my skills, I
+                          have also completed certifications in React, which
+                          highlight my ability to create interactive user
+                          interfaces efficiently. These certifications not only
+                          validate my technical knowledge but also reflect my
+                          commitment to staying updated with the latest
+                          technologies and best practices in the industry. I am
+                          dedicated to leveraging these skills to contribute
+                          effectively to any project or team I am a part of.
+                        </p>
+                        <Row>
+                          {certificate.map((project, index) => {
+                            return <ProjectCard key={index} {...project} />;
+                          })}
+                        </Row>
                       </Tab.Pane>
                       <Tab.Pane eventKey="third">
                         <p>
